d648130a46181321e75942193ca1d1de577159d0
TiagoProw <gamer23945@gmail.com>
2025-08-28 15:24:05 -0300

    Delete scripts directory

--- scripts/generate_file_index.py
+++
@@ -1,9 +0,0 @@
-import subprocess, json
-
-files = subprocess.check_output(['git','ls-files']).decode().splitlines()
-repo = "TiagoProw/lerempixels-codebase"
-branch = "main"
-out = [{"path": f, "raw_url": f"https://raw.githubusercontent.com/{repo}/{branch}/{f}"} for f in files]
-
-with open('public/file-index.json','w', encoding='utf-8') as f:
-    json.dump(out, f, indent=2, ensure_ascii=False)
